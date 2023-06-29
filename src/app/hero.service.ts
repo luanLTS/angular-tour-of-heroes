94874e940eba3d811fd8734d959fe431d5ccd1a2
Luan Teixeira <luanlts@outlook.com>
2023-06-29 16:02:54 -0300

    feat(licao04): services
    implementado services para acesso a base de heroes
    utilizado injecao de dependencia via construtor para prover o service
    para os componentes que precisam dele
    implmentado o padrao observable no heroService para realizacao de
    funcoes assincronas simulando o acesso as dados via um servidor remoto
    utiliza o ciclo de vida bgOnInit para buscar lista de heroes e deixa o
    construtor apenas para injecao de dependencia
    cria service de mensagens para notificar as operacoes que sao realidas
    no sistema e cria o componente de mensagem para mostrar essas mensagens
    no fim da tela

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root', //When you provide the service at the root level, Angular creates a single,
+  // shared instance of HeroService and injects into any class that asks for it
+})
+export class HeroService {
+  constructor(private messageService: MessageService) {}
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+}
