f3af9cfb88ea59fb468790f7d85626a33e23bd37
Luan Teixeira <luanlts@outlook.com>
2023-06-30 10:25:02 -0300

    feat(licao05): adiciona navegacao entre componentes
    implementa roteamento do angular com o RouterModule e objeto de rotas
    adiciona router-outlet para exibir o conteudo no app de acordo com o
    caminho da url
    utiliza rotas com parametros para exibir detalhes de cada hero por id
    links de navegacao com a diretiva do RouterModule routerLink

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,4 +12,23 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    // holds information about the route to this instance of the component
+    private route: ActivatedRoute,
+    // angular service for interacting with the browser, kind navigate de back page...
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    // The route.snapshot is a static image of the route information shortly after the component was created.
+    // paramsMap is a dictionary of all router parameters extracted from the URL. Thats value is always strings
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = of(HEROES.find((h) => h.id === id)!);
+    this.messageService.add('HeroService: Fetched hero with id: ' + id);
+    return hero;
+  }
 }
